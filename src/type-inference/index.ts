729d2504d29147ad051580a0a3a67ea40c0c3b77
liuchunfeng <liuchunfeng@didiglobal.com>
2020-05-07 21:02:24 +0800

    feature 类型推论

--- src/type-inference/index.ts
+++ src/type-inference/index.ts
@@ -15,3 +15,24 @@ let x2 = 3;
  */
 let x3 = [0, 1, null]
 
+/**
+ * 上下文类型
+ * 
+ * TypeScript 类型推论也可以按照相反的方向进行。这被叫做“按上下文归类”。按上下文归类会发生在表达式的类型与所处的位置相关时。比如：
+ */
+window.onmousedown = function (mouseEvent) {
+  console.log(mouseEvent.button)
+  // 这里没有报错……
+  console.log(mouseEvent.kangaroo)
+}
+
+// 这里有明确的参数类型注解，上下文类型被忽略。
+window.onmousedown = function (mouseEvent: any) {
+  console.log(mouseEvent.button)
+}
+
+// 上下文归类在很多情况下使用到。通常包括函数的参数、赋值表达式的右边、类型断言、对象成员和数组字面量和返回值语句。
+// 上下文类型也会作为最佳通用类型的候选类型，比如：
+function createZoo(): Animal[] {
+  return []
+}
